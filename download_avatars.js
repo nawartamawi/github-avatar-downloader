5d19722fdc01468b6cc67cbb537f97ea75d3aff7
nawartamawi <nawartamawi@gmail.com>
2017-04-11 02:23:25 +0000

    added downloadImage Function and test it with hardcode

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,5 @@
 const request = require("request");
-
+const fs = require("fs")
 console.log("Welcome to the Github Avatars Downloader");
 
 var GITHUB_USER = "nawartamawi";
@@ -30,3 +30,24 @@ getRepoContributors("jquery", "jquery", function(err, result){
         console.log(item.avatar_url)
     });
 });
+
+function downloadImageByURL(url, filepath) {
+request.get(url)
+    .on('error', (err) => {
+        throw err; 
+    })
+    .on('response', (response) =>{
+        console.log("Response Status Code is :", response.statusCode);
+        console.log("Repsonse Message is :", response.statusMessage);
+        console.log("Response content Type is :", response.headers['content-type'])
+    })
+    .on('end', () => {
+        console.log("Download is completed");
+    })
+    .pipe(fs.createWriteStream(filepath))
+    .on('close', () => console.log('Closed the file'))
+    ;
+        
+}
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
+
