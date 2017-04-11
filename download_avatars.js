cc0c741ae2e3586177db31ae7b2363dcbb9bf361
nawartamawi <nawartamawi@gmail.com>
2017-04-11 01:14:13 +0000

    Completed the request functions

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,19 @@
 const request = require("request");
 
 console.log("Welcome to the Github Avatars Downloader");
+
+function getRepoContributors(repoOwner, repoName, cb) { 
+    var requestUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+    request.get(requestUrl, cb)
+        .on('error', (err) => {
+            throw err; 
+        })
+        .on('response', (response) => {
+        
+        });
+
+} 
+
+getRepoContributors("jquery", "jquery", function(err, result){
+    console.log(result.body);
+});
