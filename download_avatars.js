239dda2974e1db9a2b9cff3d072d7ec05dcfb832
nawartamawi <nawartamawi@gmail.com>
2017-04-11 01:48:36 +0000

    completed Step 4 by adding getting raw data from a given repo Owner and Repo Name

--- download_avatars.js
+++ download_avatars.js
@@ -2,17 +2,27 @@ const request = require("request");
 
 console.log("Welcome to the Github Avatars Downloader");
 
+var GITHUB_USER = "nawartamawi";
+var GITHUB_TOKEN = "cdfa261b3f0e65bf413e2c9824340f784708f51f";
+
+
 function getRepoContributors(repoOwner, repoName, cb) { 
-    var requestUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`;
-    request.get(requestUrl, cb)
+    var requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+    var requestOption = {
+        headers : {
+            "user-agent" : "Nawar",
+        },
+        url : requestURL
+    }
+    request.get(requestOption, cb)
         .on('error', (err) => {
             throw err; 
         })
         .on('response', (response) => {
-        
+
         });
 
-} 
+}
 
 getRepoContributors("jquery", "jquery", function(err, result){
     console.log(result.body);
